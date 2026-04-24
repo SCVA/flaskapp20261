5b91b7ae41ef76bcb7aefffd4937364312d0de9b
Sebastian Camilo Vanegas Ayala <scva9708@gmail.com>
2026-04-23 19:02:56 -0500

    Traducir la interfaz de la aplicación al español

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   print('Request for index page received')
+   print('Solicitud de la página principal recibida')
    return render_template('index.html')
 
 @app.route('/favicon.ico')
@@ -21,10 +21,10 @@ def hello():
    name = request.form.get('name')
 
    if name:
-       print('Request for hello page received with name=%s' % name)
+       print('Solicitud de la página de saludo recibida con name=%s' % name)
        return render_template('hello.html', name = name)
    else:
-       print('Request for hello page received with no name or blank name -- redirecting')
+       print('Solicitud de la página de saludo recibida sin nombre o con nombre vacío -- redirigiendo')
        return redirect(url_for('index'))
 
 
